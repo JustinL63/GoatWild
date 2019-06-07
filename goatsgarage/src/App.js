5b94de84e002d33d120a3423590696fa42441fe7
Justin Lamb <lambjg63@gmail.com>
2019-06-07 09:47:14 -0400

    App

--- goatsgarage/src/App.js
+++ goatsgarage/src/App.js
@@ -4,20 +4,22 @@ import './App.css';
 import Customer from './Customer'
 import Form from "./Form"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-            <h1>Goat Wild's Garage</h1>
-            <Form />
-            <Customer />
+function App() {
+  return(
+    
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+              <h1>Goat Wild's Garage</h1>
+              <Form />
+              <Customer />
+  
+          </header>
+        </div>
 
-        </header>
-      </div>
-    );
-  }
+  )
+  
+  
 }
 
 export default App;
